4119d81a3684be9bc1b3f792d03496bcf5109685
Nicolas Ippoliti <contacto.nicolasippoliti@gmail.com>
2022-09-16 13:15:48 -0300

    v1.0

--- src/components/WeatherContainer/WeatherContainer.jsx
+++ src/components/WeatherContainer/WeatherContainer.jsx
@@ -34,7 +34,7 @@ function WeatherContainer() {
         return { location, error };
     };
 
-    const { location, error } = GeoLocation();
+    const { location } = GeoLocation();
 
     const [weather, setWeather] = useState({});
     
